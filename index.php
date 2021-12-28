057bf75c8fc85e759e82756ebcc8b3e5f1297ad0
melek2010 <aliyevamelek10@yahoo.com>
2021-12-28 20:04:19 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="./style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <h1 id="h1">Halcyondays</h1>
+            <button type="button" class="btn btn-primary">Back To The</button>
+        </div>
+    </header>
+
+
+    <section id="section1">
+        <h1>A Creative Portfolio Template</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias <br>magnam molestiae, cupiditate quia cum
+            enim assumenda pariatur optio dolores<br> ratione architecto dolor sapiente? Eos ducimus aperiam totam illo!
+            Reprehenderit, veritatis.
+        </p>
+    </section>
+
+
+    <section id="section2">
+        <h1>Love What You Do And You will Do It Well</h1>
+        <div class="row">
+            <div class="col-12 col-sm-4 col-xl-4">
+                <h2>Digital Marketing</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias <br>magnam molestiae, cupiditate
+                    quia cum enim assumenda pariatur optio dolores<br> ratione architecto dolor sapiente? Eos ducimus
+                    aperiam totam illo! Reprehenderit, veritatis.</p>
+            </div>
+            <div class="col-12 col-sm-4 col-xl-4">
+                <h2>Web Devlopment</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias <br>magnam molestiae, cupiditate
+                    quia cum enim assumenda pariatur optio dolores<br> ratione architecto dolor sapiente? Eos ducimus
+                    aperiam totam illo! Reprehenderit, veritatis.</p>
+            </div>
+            <div class="col-12 col-sm-4 col-xl-4">
+                <h2>Creative Donection</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias <br>magnam molestiae, cupiditate
+                    quia cum enim assumenda pariatur optio dolores<br> ratione architecto dolor sapiente? Eos ducimus
+                    aperiam totam illo! Reprehenderit, veritatis.</p>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <section id="section3">
+        <div class="col-12 col-sm-6 col-xl-6">
+            <img src="./phone.png" height="300">
+        </div>
+        <div class="col-12 col-sm-6 col-xl-6">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias <br>magnam molestiae, cupiditate
+                quia cum enim assumenda pariatur optio dolores<br> ratione architecto dolor sapiente? Eos ducimus
+                aperiam totam illo! Reprehenderit, veritatis.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias <br>magnam molestiae, cupiditate quia cum
+                enim assumenda pariatur optio dolores<br> ratione architecto dolor sapiente? Eos ducimus aperiam totam illo!
+                Reprehenderit, veritatis.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias <br>magnam molestiae, cupiditate quia cum
+                enim assumenda pariatur optio dolores<br> ratione architecto dolor sapiente? Eos ducimus aperiam totam illo!
+                Reprehenderit, veritatis.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias <br>magnam molestiae, cupiditate quia cum
+                enim assumenda pariatur optio dolores<br> ratione architecto dolor sapiente? Eos ducimus aperiam totam illo!
+                Reprehenderit, veritatis.</p>
+        </div>
+    </section>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
